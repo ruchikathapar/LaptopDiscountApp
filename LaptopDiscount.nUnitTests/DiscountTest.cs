e4355cc1dbe0251690351bd81d04532dcfe94016
hk <Rthapar6196@conestogac.on.ca>
2024-06-21 13:24:07 -0400

    Test project created

---
+++ LaptopDiscount.nUnitTests/DiscountTest.cs
@@ -0,0 +1,15 @@
+﻿namespace LaptopDiscount.nUnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
